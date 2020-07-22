19d10eefd335f938afcf4188a2081829412ee85e
faraz16iqbal <53421337+faraz16iqbal@users.noreply.github.com>
2020-07-22 16:10:02 +0530

    Changes

--- src/app.js
+++ src/app.js
@@ -6,6 +6,8 @@ const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 const app = express()
 
+const PORT = process.env.PORT || 3000
+
 // Define paths for Express config
 const publicDirectoryPath = path.join(__dirname, '../public')
 const viewsPath = path.join(__dirname, '../templates/views')
@@ -29,7 +31,7 @@ app.get('', (req, res) => {
 app.get('/about', (req, res) => {
     res.render('about', {
         title: 'About Me',
-        name: 'Andrew Mead'
+        name: 'Faraz Iqbal'
     })
 })
 
@@ -85,6 +87,6 @@ app.get('*', (req, res) => {
     })
 })
 
-app.listen(3000, () => {
-    console.log('Server is up on port 3000.')
+app.listen(PORT, () => {
+    console.log(`Server is up on port ${PORT}`)
 })
